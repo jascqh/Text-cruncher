b8295ee2c64a1c9ce1cbb3145ed78e1c473b91cc
Jasmine <jascqh@gmail.com>
2020-02-28 17:23:10 +0800

    part1

---
+++ bt3103project_part1/src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <b-container>
+      <b-row align-v = "start">
+        <slideShow></slideShow>
+      </b-row>
+      <b-row align-v = "center">
+        <b-form-input v-model="text" placeholder="Enter Quarries"></b-form-input>
+        <div class="mt-2">Value: {{ text }}</div>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+import SlideShow from "../components/slideShow.vue"
+  export default {
+    data() {
+      return {
+        text: ''
+      }
+    },
+
+    components:{
+      "slideShow": SlideShow
+    }
+  }
+</script>
