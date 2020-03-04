954b4bea67bcb02bcfe75552389ca4c11eb22be6
TzeMeng <TzeMeng@users.noreply.github.com>
2020-03-05 00:19:39 +0800

    Revert "Moving to a frontend folder"
    This reverts commit 3f31113f44b500409324d08a5d67be93c421c2c6.

---
+++ bt3103project_part1/src/components/header.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <b-navbar toggleable="lg" type="dark bg-dark static-top" variant="info">
+            <b-navbar-brand>
+                <img src="../assets/TextCruncherLogo.jpg" height ="30" width ="30">
+            </b-navbar-brand>
+            <b-navbar-brand a>TextCruncher</b-navbar-brand>
+
+            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+            <b-collapse id="nav-collapse" is-nav >
+            <b-navbar-nav class="ml-auto">
+                <b-nav-item to="/" >Home</b-nav-item>
+                <b-nav-item to="/about">About</b-nav-item>
+            </b-navbar-nav>
+            </b-collapse>
+        </b-navbar>
+    </div>
+
+</template>
+
+<style lang="scss" scoped>
+#nav a.router-link-exact-active {
+    color: white;
+}
+#nav a{
+    font-weight: bold;
+    color:gray;
+
+}
+
+</style>
---
+++ bt3103project_part1/src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+<div>
+  <b-container align="center">
+      <slideShow align="center"></slideShow>
+      <p>Please input at least one query up to 5 at once.</p>
+    <b-row align-v = "center">
+      <b-form-textarea v-model="text"
+      :placeholder="'Enter your queries here \nEg. Quary 1, Quary 2, Quary 3 ... Up to 5 Quaries'"
+      :rows="2"
+      :max-rows="5"></b-form-textarea>
+    </b-row>
+      <div class="mt-2" align="center">Please separate your queries with a comma ',' in between</div>
+      <div class="mt-2">Value: {{ text }}</div>
+  </b-container>
+</div>
+</template>
+
+<script>
+import SlideShow from "../components/slideShow.vue"
+export default {
+  data() {
+    return {
+      text: ''
+    }
+  },
+
+  components:{
+    "slideShow": SlideShow
+  }
+}
+</script>
