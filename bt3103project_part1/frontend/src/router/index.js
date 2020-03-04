756c48399f531a1093e1089fbdbfd4b1a77d5b90
TzeMeng <TzeMeng@users.noreply.github.com>
2020-03-04 23:44:09 +0800

    re-adding bootstrap to frontend

--- bt3103project_part1/frontend/src/router/index.js
+++ bt3103project_part1/frontend/src/router/index.js
@@ -1,11 +1,11 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
-import About from"../views/About.vue";
+// import Home from "../views/Home.vue";
+// import About from"../views/About.vue";
 
 Vue.use(VueRouter);
 
-const routes = [
+const routerOptions  = [
   {
     path: "/",
     name: "Home",
@@ -14,18 +14,19 @@ const routes = [
   {
     path: "/about",
     name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: About
-    // () =>
-    //   import(/* webpackChunkName: "about" */ "../views/About.vue")
   }
-];
+]
+
+const routes = routerOptions.map(route => {
+  return {
+      ...route,
+      componenet: () => import ('@/components/${route.component}.vue')
+  }
+})
 
 const router = new VueRouter({
   mode: "history",
-  base: process.env.BASE_URL,
   routes
 });
 
