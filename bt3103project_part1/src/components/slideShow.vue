80b8245299e637b5360e7b228698806b3e7d387d
TzeMeng <TzeMeng@users.noreply.github.com>
2020-03-11 17:24:21 +0800

    Forgot to git.ignore enviro

---
+++ bt3103project_part1/src/components/slideShow.vue
@@ -0,0 +1,42 @@
+<template>
+<div class="col-lg-6" align="center">
+    <div class="col-sm-6" align="center">
+        <b-carousel
+            v-model="slide"
+            :interval="4000"
+        >
+            <b-carousel-slide>
+                <template v-slot:img>
+                <img
+                    class="d-block w-200"
+                    src="../assets/TextCruncherLogo.jpg"
+                    alt="image slot"
+                    height="200"
+                >
+                </template>
+            </b-carousel-slide>
+
+            <b-carousel-slide>
+                <template v-slot:img>
+                <img
+                    class="d-block w-200"
+                    src="../assets/slide2.png"
+                    alt="image slot"
+                >
+                </template>
+            </b-carousel-slide>
+                        <b-carousel-slide>
+                <template v-slot:img>
+                <img
+                    class="d-block w-200"
+                    src="../assets/slide3.png"
+                    alt="image slot"
+                >
+                </template>
+            </b-carousel-slide>
+        </b-carousel>
+    </div>
+    </div>
+</template>
+
+
