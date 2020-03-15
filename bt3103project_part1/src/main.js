06f5bf5fd6b2f465c724828c24c62b12c606cdf1
Jasmine <jascqh@gmail.com>
2020-03-16 03:38:09 +0800

    connected to firebase

--- bt3103project_part1/src/main.js
+++ bt3103project_part1/src/main.js
@@ -5,7 +5,10 @@ import "./plugins/bootstrap-vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import firebase from './firebase.js';
 
+/* Bind firebase to your Vue instance */
+Vue.prototype.$firebase = firebase;
 Vue.config.productionTip = false;
 
 new Vue({
