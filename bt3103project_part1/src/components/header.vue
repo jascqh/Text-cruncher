0f23892d3e24024077aae7d50e3dd4a7f9e5b5d3
Samantha <samanthaaaalim@gmail.com>
2020-03-26 13:41:29 +0800

    Edited formatting for login and register

--- bt3103project_part1/src/components/header.vue
+++ bt3103project_part1/src/components/header.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <b-navbar toggleable="lg" type="dark bg-dark static-top" variant="info">
+        <b-navbar toggleable="lg" type="dark bg-dark static-top" variant="info" >
             <b-navbar-brand>
                 <img src="../assets/TextCruncherLogo.jpg" height ="30" width ="30">
             </b-navbar-brand>
@@ -8,13 +8,14 @@
             <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
             <b-collapse id="nav-collapse" is-nav >
             <b-navbar-nav class="ml-auto">
-                <a class="nav-link disabled" href="#">{{this.$session.get('email')}}</a>
+               <a class="nav-link disabled" href="#">{{this.$session.get('email')}}</a> 
+               <!-- cannot get this to appear :c because navbar not being refreshed -->
                 <b-nav-item to="/" >Home</b-nav-item>
                 <b-nav-item to="/about">About</b-nav-item>
                 <b-nav-item to="/login">Login</b-nav-item>
                 <b-nav-item to="/register">Register</b-nav-item>
                 <b-nav-item to="/history">History</b-nav-item>
-                <li><button v-on:click="logout" class="btn btn-large btn-extended grey lighten-4 black-text">Logout</button></li>
+                <li><button v-on:click="logout" class="btn btn-outline-light btn-sm">Logout</button></li>
             </b-navbar-nav>
             </b-collapse>
         </b-navbar>
