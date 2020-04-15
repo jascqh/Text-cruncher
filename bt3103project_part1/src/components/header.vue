6acf7459d2b3b95e0ae8726f18b40b39992f129b
TzeMeng <TzeMeng@users.noreply.github.com>
2020-04-15 15:39:49 +0800

    Revert "uploaded my src"
    This reverts commit 6dddf5cd34c00aa8a22854fcf866afd01c19005f.

--- bt3103project_part1/src/components/header.vue
+++ bt3103project_part1/src/components/header.vue
@@ -1,18 +1,25 @@
 <template>
     <div>
-        <b-navbar toggleable="lg" type="light" >
+        <b-navbar toggleable="lg" type="dark bg-dark static-top" variant="info">
+            <b-navbar-brand>
+                <img src="../assets/TextCruncherLogo.jpg" height ="30" width ="30">
+            </b-navbar-brand>
+            <b-navbar-brand a>TextCruncher</b-navbar-brand>
+            
             <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
             <b-collapse id="nav-collapse" is-nav >
-              <b-navbar-nav class="ml-auto">
+            <b-navbar-nav class="ml-auto">
                 <b-nav-item to="/" >Home</b-nav-item>
                 <b-nav-item to="/about">About</b-nav-item>
                 <b-nav-item to="/login">Login</b-nav-item>
                 <b-nav-item to="/register">Register</b-nav-item>
                 <b-nav-item to="/history">History</b-nav-item>
-              </b-navbar-nav>
+            </b-navbar-nav>
             </b-collapse>
         </b-navbar>
-      </div>
+    </div>
+
 </template>
 
 <script>
@@ -46,15 +53,12 @@ export default {
 
 <style lang="scss" scoped>
 #nav a.router-link-exact-active {
-    color: #4682B4;
+    color: white;
 }
 #nav a{
-    font-weight:100;
+    font-weight: bold;
     color:gray;
-}
 
-.navbar-default {
-  background: rgba(0,0,0,0.5);
 }
 
 </style>
