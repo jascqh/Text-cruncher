3e3043f5d9409dec7b50ef06f5628725e41ab8a0
TzeMeng <TzeMeng@users.noreply.github.com>
2020-04-15 15:39:27 +0800

    Revert "Revert "Update header.vue""
    This reverts commit 1e7b2fe369f5d131fa100540d0f5f5b17904bb3c.

--- bt3103project_part1/src/components/header.vue
+++ bt3103project_part1/src/components/header.vue
@@ -1,15 +1,23 @@
 <template>
     <div>
-        <b-navbar toggleable="lg" type="light" >
+        <b-navbar toggleable="lg" type="dark bg-dark static-top" variant="info" >
+            <b-navbar-brand>
+                <img src="..\assets\TextCruncher.png" height="30" width="30">
+            </b-navbar-brand>
+            <b-navbar-brand a>TextCruncher</b-navbar-brand>
             <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
             <b-collapse id="nav-collapse" is-nav >
-              <b-navbar-nav class="ml-auto">
+            <b-navbar-nav class="ml-auto">
+               <a class="nav-link disabled" href="#">{{this.$session.get('email')}}</a> 
+               <!-- cannot get this to appear :c because navbar not being refreshed -->
                 <b-nav-item to="/" >Home</b-nav-item>
                 <b-nav-item to="/about">About</b-nav-item>
-                <b-nav-item to="/login">Login</b-nav-item>
-                <b-nav-item to="/register">Register</b-nav-item>
+                <!--<b-nav-item to="/login">Login</b-nav-item>
+                <b-nav-item to="/register">Register</b-nav-item> -->
                 <b-nav-item to="/history">History</b-nav-item>
-              </b-navbar-nav>
+                <b-nav-item to="/download">Download</b-nav-item>
+                <li><button v-on:click="logout" class="btn btn-outline-light btn-sm">Logout</button></li>
+            </b-navbar-nav>
             </b-collapse>
         </b-navbar>
       </div>
@@ -18,7 +26,7 @@
 <script>
 import firebase from 'firebase';
 export default {
-  name: 'navbar',
+  name: 'header',
   data() {
     return {
       isLoggedIn: false,
@@ -29,15 +37,18 @@ export default {
     if (firebase.auth().currentUser) {
       this.isLoggedIn = true;
       this.currentUser = firebase.auth().currentUser.email;
+      console.log(this.currentUser); 
     }
   },
   methods: {
     logout: function() {
+      this.$session.clear("email");
       firebase
         .auth()
         .signOut()
         .then(() => {
-          this.$router.go({ path: this.$router.path });
+          this.$router.replace({ path: '/login'});
+          this.$router.go({ path: '/login'});
         });
     }
   }
