6dddf5cd34c00aa8a22854fcf866afd01c19005f
jiayingyapyap <61984444+jiayingyapyap@users.noreply.github.com>
2020-04-15 14:47:31 +0800

    uploaded my src

--- bt3103project_part1/src/views/Login.vue
+++ bt3103project_part1/src/views/Login.vue
@@ -1,29 +1,69 @@
 <template>
-  <div>
-    <div class="container">
-    <div class="row">
-      <div class="col s12 m8 offset-m2">
-        <div class="login card-panel grey white-text center">
-          <h3>Login</h3>
-          <form action="index.html">
-            <div class="input-field">
-              <i class="material-icons prefix">email</i>
-              <input type="email" id="email" v-model="email">
-              <label class="white-text" for="email">Email Address</label>
-            </div>
-            <div class="input-field">
-              <i class="material-icons prefix">lock</i>
-              <input type="password" id="password" v-model="password">
-              <label class="white-text" for="password">Password</label>
-            </div>
-            <button v-on:click="login" class="btn btn-large btn-extended grey lighten-4 black-text">Login</button>
-          </form>
+  <div> 
+  <b-card title="Login"
+  style="max-width: 30rem;"
+  class="mb2">
+    <span style="display:inline-block; width: YOURWIDTH;"></span>
+    <b-card-text>
+      <form action="index.html">
+        <div class="input">
+          <input class= "form-box" type="email" v-model="email" placeholder="Email Address">
         </div>
-      </div>
-    </div>
-  </div>
-  </div>
+        <div>
+          <input class= "form-box" type="password" v-model="password" placeholder="Password" >
+        </div>
+
+        <div>
+         <a class="btn-signin" v-on:click="login">Log In</a>
+        </div>
+      </form>
+    </b-card-text>
+  </b-card>
+</div>
 </template>
+
+
+
+<style scoped>
+.card{
+  margin: 0px auto;
+  float:none;
+  margin-bottom:10px ;
+}
+
+::placeholder{
+  color:darkgrey;
+  opacity: 1;
+  font-size: 12px;
+}
+
+.form-box{
+  width:100%;
+  height: 40px;
+  padding-left: 15px;
+  border: none;
+  border-radius:20px;
+  margin-bottom: 20px;
+  background: #eeeeee;
+}
+
+
+.btn-signin {
+  float: left;
+  font-weight: 700;
+  text-transform: uppercase;
+  font-size: 13px;
+  text-align: center;
+  color: #ffffff;
+  padding-top: 8px;
+  width: 40%;
+  height: 35px;
+  border: none;
+  border-radius: 18px;
+  margin-top: 20px;
+  background-color: #4682B4;
+}
+</style>
 
 <script>
 import firebase from 'firebase';
@@ -54,3 +94,4 @@ export default {
   }
 };
 </script>
+
