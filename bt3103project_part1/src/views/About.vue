873ceca8a482aa9eba98922f43d499fb3058a68e
Sniub <50547736+Sniub@users.noreply.github.com>
2020-04-15 20:47:23 +0800

    Update About.vue

--- bt3103project_part1/src/views/About.vue
+++ bt3103project_part1/src/views/About.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about">
-    <h1>Dashboard</h1> 
+    <h3 class = "dash">Some Insights...</h3>  
     
     <div id = "chart">
       <barchart></barchart>
@@ -15,10 +15,12 @@
     <div id = "chart2">
       <runtime></runtime>
     </div>
+    <VueFaqAccordion class = "FAQ" :items="myItems"/>
   </div>
 </template>
 
 <script>
+import VueFaqAccordion from 'vue-faq-accordion'
 import barchart from '../barchart.js'
 import linechart from '../linechart.js'
 import doughnut from '../doughnut.js'
@@ -26,11 +28,54 @@ import runtime from '../runtime.js'
 
 export default {
   components:{
-    barchart, linechart, doughnut, runtime
-  }
+    barchart, linechart, doughnut, runtime, VueFaqAccordion
+  },
+  data() {
+    return {
+      myItems : [
+        {
+          title: "What is Text Cruncher?",
+          value: "Text Cruncher is a solution provider for students, professors and researchers or anybody in general who are spending hours doing research for their projects. "
+              + "We access the searched content from Google search results and help our users scrape all the data to "
+              + "produce a compiled excel sheet of the relevant summarized information that can either be downloaded instantly"
+              + "through the website or sent via email to the user."
+        },
+        {
+          title: "How much does it cost to use Text Cruncher?",
+          value: "It is entirely free of charge to use Text Cruncher."
+        },
+        {
+          title:"How many queries can I search at one go?",
+          value:  "Up to five queries, with each being separated by a comma.\n"
+              +"Example with 3 queries:  “Dog,Cat,Tails” \n"
+              + "Example with 4 queries: “Dog Food, Canned Food, Animal Control, Cats and Dogs”\n"
+        },
+        {
+          title:"Can I use Text Cruncher without an account?",
+          value: "Yes. You can download the summarized excel sheet onto the device you are using or email it to a preferred email."
+        },
+        {
+          title: "What features does the account have?",
+          value: "Having an account allows the user to store their previous searches in the account history which allows them to refer back to their previous searches whenever they need to." 
+        },
+        {
+          title:"There is no available file for download. What can I do?",
+          value:"As Text Cruncher aims to optimise the research process by filtering searches that contained the all queries inputted. "
+            +"If no recommended file shows up, it is likely that there are no trustworthy sites that contain all the queries. "
+            +"Users can try searching again with fewer keywords/queries."
+        },
+        {
+          title:"I am having trouble downloading. Who can I look for?",
+          value:"            For all troubles/feedback with regards to Text Cruncher, please call the following hotline: +65 6235 35335."
+            +"Our operating hours are 8am-6pm weekdays 10am-4pm weekends. Please note as Text Cruncher is a work in progress, " 
+            +"we welcome your comments to help this platform grow further and appreciate your kind patience."
+        }
+      ]
+    }
+  },
 }
 
-</script>>
+</script>
 
 <style scoped>
 #main{
@@ -52,4 +97,17 @@ export default {
   margin:auto;
   float:left;
 }
-</style>>
+.dash{
+  padding-top: 1cm;
+  font-family: "Lucida Console", Courier, monospace;
+}
+.FAQ{
+  right: 150px;
+}
+/* Clear floats after the columns */
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+</style>
