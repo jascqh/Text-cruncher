a34ca72e7922619b034656feaaa3250c08fa1d20
TzeMeng <TzeMeng@users.noreply.github.com>
2020-04-21 14:06:00 +0800

    Retrive from Firebase

--- bt3103project_part1/src/views/History.vue
+++ bt3103project_part1/src/views/History.vue
@@ -3,7 +3,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <b-card class="card">
     <h1>Search history</h1>
-      <ul>
+      <!-- <ul>
         <li v-for="(item , index) in itemsList" v-bind:key="item.id" v-on="increaseCount()">
           <div>
             {{item.Name}}
@@ -18,6 +18,9 @@
             
            
         </li>
+    </ul> -->
+    <ul id="item-list">
+
     </ul>
     </b-card>
 </div>
@@ -29,29 +32,68 @@ import database from '../firebase.js'
 export default {
   data(){
     return{
-        itemsList: [],
+        // itemsList: [],
         email: "",
         count:0
         }
   },
   methods:{
-    fetchItems:function(){
-      this.email = this.$session.get('email')      
-      let item={}
-      //Get all the items from DB
-      database.collection('files').get().then((querySnapShot)=>{
-        //Loop through each item
-        querySnapShot.forEach(doc=>{
-          if (this.email== doc.data().Email){
-            item=doc.data()
-            item.id=doc.id
-            this.itemsList.push(item)
-            item = {}
-          }
-        })
-      })
-      
+
+    renderData : function(doc){
+      this.email = this.$session.get('email') 
+      if (this.email== doc.data().Email) {
+        console.log(doc.data())
+        let itemList = document.querySelector('#item-list');
+        
+        let li = document.createElement('li');
+        let name = document.createElement('span');
+
+        li.setAttribute('data-id', doc.id);
+        name.textContent = doc.data().Name;
+        
+        li.appendChild(name);
+
+        itemList.appendChild(li);
+      }
+
     },
+
+    fetchItems: function() {
+      let itemList = document.querySelector('#item-list');
+      database.collection('files').onSnapshot(snapshot => {
+      let changes = snapshot.docChanges();
+      changes.forEach(change => {
+          // console.log(change.doc.data());
+          if(change.type == 'added'){
+              // console.log("Adding");
+              this.renderData(change.doc);
+          } else if (change.type == 'removed'){
+              let li = itemList.querySelector('[data-id=' + change.doc.id + ']');
+              itemList.removeChild(li);
+          }
+          });
+        });
+    },
+    // fetchItems:function(){
+    //   let myList = document.querySelector('item-list');
+    //   this.email = this.$session.get('email')      
+    //   let allItem=[]
+    //   let item = {}
+
+    //   //Get all the items from DB
+    //   database.collection('files').get().then((querySnapShot)=>{
+    //     //Loop through each item
+    //     querySnapShot.forEach(doc=>{
+    //       if (this.email== doc.data().Email){
+    //         item=doc.data()
+    //         item.id=doc.id
+    //         allItem.push(item)
+    //         item = {}
+    //       }
+    //     })
+    //   })
+    //   return allItem
+    // },
     deleteItem:function(index,item){
       //Deleting from DB
       database.collection('files').doc(item.id).delete()
@@ -153,5 +195,7 @@ li:nth-of-type(even) {
 .card{
   margin:30px
 }
+
+
 
 </style>
